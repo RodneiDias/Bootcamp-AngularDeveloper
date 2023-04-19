335ddc47398b9627f6d0b759bb710579ed1bae65
Rodnei Dias <rodnei.dias4@gmail.com>
2023-04-19 20:44:56 -0300

    inserindo mais um método dentro da classe

--- javascript-modulo1/5- objetos e classes/aula5-praticando-2.js
+++ javascript-modulo1/5- objetos e classes/aula5-praticando-2.js
@@ -20,7 +20,22 @@ class Pessoa {
     calcImc(){
         return (this.peso / Math.pow(this.altura, 2)).toFixed(2);
     }
+    classificaImc(){
+        const imc = this.calcImc();
+        if(imc < 18.5) {  
+            return('Abaixo do peso');
+        }else if(imc >=18.5 && imc < 25){
+            return('Peso normal');
+        }else if(imc >=25 && imc < 30){
+            return('Acima do peso');
+        }else if(imc >=30 && imc < 40){
+            return('Obeso');
+        }
+        else {
+            return('Obesidade morbida');
+        }
+    }
 }
 
 const jose = new Pessoa('jose', 70, 1.75);
-console.log(jose.calcImc());
+console.log(jose.calcImc(), jose.classificaImc());
