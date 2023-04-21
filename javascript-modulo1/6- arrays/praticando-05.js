0064e17c407396cc313c07eb19334ed563fe48aa
Rodnei Dias <rodnei.dias4@gmail.com>
2023-04-21 14:36:32 -0300

    imprimindo medias a baixo de 5

--- javascript-modulo1/6- arrays/praticando-05.js
+++ javascript-modulo1/6- arrays/praticando-05.js
@@ -3,3 +3,11 @@ Dado uma lista com as médias tiradas pelos alunos.
 Imprima todas as médias que ficaram de recuperação (média < 5).
 Ex de lista = [2, 7, 3, 8, 10, 4]
 * */
+let listMedias = [2, 7, 3, 8, 10, 4];
+
+for (let i = 0; i < listMedias.length; i++) {
+    const element = listMedias[i];
+    if(element < 5){
+        console.log(element)
+    }       
+}
