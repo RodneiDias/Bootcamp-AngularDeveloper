bac771bdb240c32799bd7d2db2363f901e25b4e5
Rodnei Dias <rodnei.dias4@gmail.com>
2023-04-21 14:08:14 -0300

    ler o primeiro caractere da string charAt(), usando o filter() para gegar o novo array

--- javascript-modulo1/6- arrays/praticando-03.js
+++ javascript-modulo1/6- arrays/praticando-03.js
@@ -1,6 +1,17 @@
 /*
- Faça um programa que dado uma lista de nomes imprima os nomes que começem com a letra "V".
+ Faça um programa que dado uma lista de nomes imprima os 
+ nomes que começem com a letra "V"?
 * */
+let listNomes = ['rodnei','valeria','monica','veronica'];
+
+function retornaNomes(nome){
+    if (nome.charAt(0) == "v") 
+    return nome;
+}
+var resultado = listNomes.filter(retornaNomes);
+console.log(resultado);
+
+
 
 
 
