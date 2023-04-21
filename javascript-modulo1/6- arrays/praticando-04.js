7af116bfcc96d6370ee9596070f4e0a6378bf89e
Rodnei Dias <rodnei.dias4@gmail.com>
2023-04-21 14:28:36 -0300

    usando while de 10 a 50 e push() para preencher o array com os resultados

--- javascript-modulo1/6- arrays/praticando-04.js
+++ javascript-modulo1/6- arrays/praticando-04.js
@@ -1,3 +1,15 @@
 /*
-Faça um programa que preencha uma lista com todos os números pares contidos no intervalo de 10 a 50;
+Faça um programa que preencha uma lista com todos os números 
+pares contidos no intervalo de 10 a 50;
 * */
+
+let numerosPares = [];
+let cont =10 ;
+
+    while (cont<50) {
+        let resto = cont%2;
+      
+        cont = cont + 2;
+        numerosPares.push(cont)
+    }
+    console.log(numerosPares)
